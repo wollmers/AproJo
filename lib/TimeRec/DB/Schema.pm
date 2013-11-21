99f2b80b4da30dd26390daac973f2538978c617d
Helmut Wollmersdorfer <helmut.wollmersdorfer@fixpunkt.de>
2013-11-21 16:06:18 +0100

    added DB::Schema

--- lib/TimeRec/DB/Schema.pm
+++ lib/TimeRec/DB/Schema.pm
@@ -1,7 +1,19 @@
 package TimeRec::DB::Schema;
-use base qw/DBIx::Class::Schema/;
 
-__PACKAGE__->load_namespaces();
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
 
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07000 @ 2013-11-21 15:57:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Mrj7nszrbPS6R17yk3OPbw
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
-
--- lib/TimeRec/DB/Schema/Result/User.pm
+++ lib/TimeRec/DB/Schema/Result/User.pm
@@ -1,40 +1,106 @@
 package TimeRec::DB::Schema::Result::User;
 
-use DBIx::Class::Candy
-  -autotable => v1,
-  -components => [ qw/ EncodedColumn / ];
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
 
-primary_column user_id => {
-  data_type => 'INT',
-  is_auto_increment => 1,
-};
+use strict;
+use warnings;
 
-unique_column name => { 
-  data_type => 'VARCHAR',
-};
+use base 'DBIx::Class::Core';
 
-column full => {
-  data_type => 'VARCHAR',
-};
 
-column password => {
-    data_type => 'VARCHAR',
-    encode_column => 1,
-    encode_class  => 'Crypt::Eksblowfish::Bcrypt',
-    encode_check_method => 'check_password',
-};
+=head1 NAME
 
-column is_author => { 
-  data_type => 'BOOL',
-  default_value => 0,
-};
+TimeRec::DB::Schema::Result::User
 
-column is_admin => { 
-  data_type => 'BOOL',
-  default_value => 0,
-};
+=cut
 
-has_many pages => 'TimeRec::DB::Schema::Result::Page', 'author_id';
+__PACKAGE__->table("users");
 
+=head1 ACCESSORS
+
+=head2 userID
+
+  accessor: 'user_id'
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 name
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 160
+
+=head2 alias
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 160
+
+=head2 trash
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 active
+
+  data_type: 'tinyint'
+  default_value: 1
+  is_nullable: 0
+
+=head2 mail
+
+  data_type: 'varchar'
+  default_value: (empty string)
+  is_nullable: 0
+  size: 100
+
+=head2 password
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 254
+
+=head2 globalRoleID
+
+  accessor: 'global_role_id'
+  data_type: 'integer'
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "userID",
+  {
+    accessor          => "user_id",
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+  },
+  "name",
+  { data_type => "varchar", is_nullable => 0, size => 160 },
+  "alias",
+  { data_type => "varchar", is_nullable => 1, size => 160 },
+  "trash",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+  "active",
+  { data_type => "tinyint", default_value => 1, is_nullable => 0 },
+  "mail",
+  { data_type => "varchar", default_value => "", is_nullable => 0, size => 100 },
+  "password",
+  { data_type => "varchar", is_nullable => 1, size => 254 },
+  "globalRoleID",
+  { accessor => "global_role_id", data_type => "integer", is_nullable => 0 },
+);
+__PACKAGE__->set_primary_key("userID");
+__PACKAGE__->add_unique_constraint("name", ["name"]);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07000 @ 2013-11-21 15:57:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:KDfAhs/skd8IzknEkVPv3A
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
-
