8a44857265a140c689e093f83be28d1bd4972e08
wollmers <helmut.wollmersdorfer@fixpunkt.de>
2013-12-02 16:50:47 +0100

    moved skins, added translation

---
+++ lib/TimeRec/I18N/en.pm
@@ -0,0 +1,37 @@
+package TimeRec::I18N::en;
+use Mojo::Base 'TimeRec::I18N';
+
+our %Lexicon = (
+
+'start' => 'Start',
+'logout from system' => 'logout from system',
+'log into system' => 'log into system',
+'change skin' => 'change skin',
+
+'login' => 'Login',
+'logout' => 'Logout',
+
+'The universal Time Tracking Tool by Fixpunkt' => 'The universal Time Tracking Tool by Fixpunkt',
+
+'Login' => 'Login',
+'username' => 'Username',
+'password' => 'Password',
+'keep logged in' => 'keep me logged in',
+'lost password?' => 'lost password?',
+'register newly' => 'register newly',
+
+'user_id' => 'User-ID',
+'alias' => 'Alias',
+'action' => 'Action',
+'edit' => 'Edit',
+
+'Set a New Password' => 'Set a New Password',
+'New Password' => 'New Password',
+'Repeat Password' => 'Repeat Password',
+'Only fill these boxes to set a new password (required for new account).' => 'Only fill these boxes to set a new password (required for new account).',
+'Administrators can add and change users' => 'Administrators can add and change users',
+'store' => 'store',
+
+);
+
+1;
